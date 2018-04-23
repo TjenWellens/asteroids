5c66ad4df2c1d3973d6e1fb1dfd59abd392519c6
Tjen Wellens <(none)>
2018-04-23 11:07:27 +0200

    expanded clock

--- src/Views/Clock.elm
+++ src/Views/Clock.elm
@@ -5,21 +5,24 @@ import Time exposing (Time, second)
 
 import Svg exposing (..)
 
+type alias Unit = (Time -> Float)
+
 clock: Time -> List (Svg msg)
 clock time =
     [ clockCircle time
-    , clockHand time
+    , clockHand time Time.inMinutes
+    , clockHand time Time.inHours
     ]
 
 clockCircle: Time -> Svg msg
 clockCircle time =
     circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
 
-clockHand: Time -> Svg msg
-clockHand time =
+clockHand: Time -> Unit -> Svg msg
+clockHand time handType =
     let
       angle =
-        turns (Time.inMinutes time)
+        turns (handType time)
 
       handX =
         toString (50 + 40 * cos angle)
