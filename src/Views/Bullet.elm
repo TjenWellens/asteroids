a42521cefe54726d9f0c544c02329c39895623f2
Tjen Wellens <(none)>
2018-04-26 11:40:45 +0200

    refactor View.Bullet to not use dx dy

--- src/Views/Bullet.elm
+++ src/Views/Bullet.elm
@@ -7,12 +7,11 @@ import Svg.Attributes exposing (..)
 bullet : Bullet -> Svg msg
 bullet bullet =
     let
-        x_1 = toString bullet.position.x
-        y_1 = toString bullet.position.y
-        x_2 = toString (bullet.position.x - bullet.momentum.heading.dx)
-        y_2 = toString (bullet.position.y - bullet.momentum.heading.dy)
+        x = toString bullet.position.x
+        y = toString bullet.position.y
+        radius = "0.7"
     in
-        line [x1 x_1, y1 y_1, x2 x_2, y2 y_2, stroke "#aaaaaa"] []
+        circle [ cx x, cy y, r radius, fill "#aaaaaa" ] []
 
 bullets : List Bullet -> List (Svg msg)
 bullets bullets =
